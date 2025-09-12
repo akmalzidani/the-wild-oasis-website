01425b5a2941ee8b24d9dbbad73401ce8cafb354
akmalzidani <akmal20031003@gmail.com>
2025-09-12 22:55:16 +0700

    feat: integrate Josefin Sans font into layout and update body className

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,12 @@ import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -16,7 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950 text-primary-50">
+      <body
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-50`}
+      >
         <header>
           <Logo />
         </header>
