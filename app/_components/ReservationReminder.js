69b896070453b9d87f027c8dd05bf58ed32eeafd
akmalzidani <akmal20031003@gmail.com>
2025-09-19 18:18:08 +0700

    feat: enhance DateSelector and ReservationReminder components with optional chaining for safer property access

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -7,14 +7,14 @@ import { useReservation } from "./ReservationContext";
 function ReservationReminder() {
   const { range, resetRange } = useReservation();
 
-  if (!range.from || !range.to) return null;
+  if (!range?.from || !range?.to) return null;
 
   return (
     <div className="fixed flex items-center gap-8 px-8 py-5 font-semibold -translate-x-1/2 rounded-full shadow-xl bottom-6 left-1/2 bg-accent-500 text-primary-800 text shadow-slate-900">
       <p>
         <span>👋</span> Don&apos;f forget to reserve your dates <br /> from{" "}
-        {format(new Date(range.from), "MMM dd yyyy")} to{" "}
-        {format(new Date(range.to), "MMM dd yyyy")}
+        {format(new Date(range?.from), "MMM dd yyyy")} to{" "}
+        {format(new Date(range?.to), "MMM dd yyyy")}
       </p>
       <button
         className="p-1 transition-all rounded-full hover:bg-accent-600"
