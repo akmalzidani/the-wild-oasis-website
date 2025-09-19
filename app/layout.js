5ee4d5e63baf05d86ad76b4c952ac86ca95449b3
akmalzidani <akmal20031003@gmail.com>
2025-09-19 21:36:19 +0700

    feat: add SpeedInsights component to RootLayout for performance monitoring

--- app/layout.js
+++ app/layout.js
@@ -33,6 +33,7 @@ export default function RootLayout({ children }) {
             <ReservationProvider>{children}</ReservationProvider>
           </main>
         </main>
+        <SpeedInsights />
       </body>
     </html>
   );
