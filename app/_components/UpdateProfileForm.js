439792a19bf9ed62e70be4e26e686517c08def9d
akmalzidani <akmal20031003@gmail.com>
2025-09-19 14:28:48 +0700

    feat: refactor UpdateProfileForm to use Button component with pending state handling

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { updateProfile } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
 export default function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState(0);
@@ -55,10 +56,17 @@ export default function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex items-center justify-end gap-6">
-        <button className="px-8 py-4 font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+  return (
+    <button className="px-8 py-4 font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300" disabled={pending}>
+      {pending ? 'Updating...' :"Update profile"}
+    </button>
   );
 }
